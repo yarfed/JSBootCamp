03bd390684474aa576293bde6493685c51a4a37d
Yaroslav Fedorov <yarfed77@gmail.com>
2016-02-18 15:38:59 +0200

    some refactoring, work version.

--- 01 JS Programming/main.js
+++ 01 JS Programming/main.js
@@ -5,138 +5,157 @@
 var readlineSync = require('readline-sync');
 var lastId = 1;
 var contacts = [];
-var rootGroup = {id: 0, name: "root"};
+var ROOT_GROUP_ID = 0;
+var rootGroup = {id: ROOT_GROUP_ID, name: 'root'};
 var currentGroup = rootGroup;
 var groups = [rootGroup];
-var MENU_ADD_CONTACT = "1";
-var MENU_ADD_GROUP = "2";
-var MENU_CHANGE_CURRENT_GROUP = "3";
-var MENU_PRINT_CURRENT_GROUP = "4";
-var MENU_PRINT_ALL = "5";
-var MENU_FIND = "6";
-var MENU_DELETE_ITEM = "7";
-var MENU_EXIT = "8";
-groups.push({id: 1, name: "qqqqqq", parentId: 0});
-groups.push({id: 2, name: "aaa", parentId: 0});
-groups.push({id: 3, name: "hhhhh", parentId: 2});
-groups.push({id: 4, name: "qqdddq", parentId: 3});
-groups.push({id: 5, name: "qvbfjjfqq", parentId: 2});
-contacts.push({id: 6, firstName: 'aaa', lastName: 'vasya1', phone: 12345, groupId: 0});
-contacts.push({id: 7, firstName: 'petya2', lastName: 'vasya2', phone: 14445, groupId: 2});
-contacts.push({id: 8, firstName: 'petya3', lastName: 'vasya3', phone: 555545, groupId: 2});
-lastId = 9;
+var MENU_ADD_CONTACT = '1';
+var MENU_ADD_GROUP = '2';
+var MENU_CHANGE_CURRENT_GROUP = '3';
+var MENU_PRINT_CURRENT_GROUP = '4';
+var MENU_PRINT_ALL = '5';
+var MENU_FIND = '6';
+var MENU_DELETE_ITEM = '7';
+var MENU_EXIT = '8';
+
 runPhoneBook();
 
-
 function runPhoneBook() {
-    console.log("hello");
+    console.log('hello');
     do {
         printMenu();
-        var inputString = readlineSync.question('enter you choose, please!');
-        switch (inputString) {
+        var input= readlineSync.question('enter you choose, please!');
+        switch (input) {
             case MENU_ADD_CONTACT:
-                inputString = readlineSync.question('first name, last name, telephones separates with "," ');
-                var contact = createContactFromString(inputString);
-                contacts.push(contact);
+                var firstName = inputNonEmptyField('enter first name ');
+                var lastName = inputNonEmptyField('enter last name ');
+                var telephones = inputNonEmptyField('enter telephones separate with "," ').split(',');
+                contacts.push(createNewContact(firstName, lastName, telephones));
                 printGroup(currentGroup);
                 break;
 
             case MENU_ADD_GROUP:
-                input = readlineSync.question('please enter group name ');
-                addNewGroup(input);
-                printGroup(currentGroupId);
+                do {
+                    var groupName = inputNonEmptyField('please enter group name ');
+                    if (isDoubleName(groupName)) {
+                        console.log('cant be double names in group');
+                    }
+                }
+                while (isDoubleName(groupName));
+                groups.push(createNewGroup(groupName));
+                printGroup(currentGroup);
                 break;
 
             case MENU_CHANGE_CURRENT_GROUP:
-                input = readlineSync.question('please enter group name or .. to upLevel ');
+                printGroup(currentGroup);
+                input = inputNonEmptyField('please enter group name or .. to upLevel ');
                 changeCurrentGroup(input);
-                printGroup(currentGroupId);
+                printGroup(currentGroup);
                 break;
 
             case MENU_PRINT_CURRENT_GROUP:
-                printGroup(currentGroupId);
+                printGroup(currentGroup);
                 break;
 
             case MENU_PRINT_ALL:
-                printChildrens(0, "  ");
+                printChildrens(getGroupById(ROOT_GROUP_ID), "  ");
+                console.log('you in '+ currentGroup.name + ' now');
                 break;
 
             case MENU_FIND:
-                inputString = readlineSync.question('please enter string for search');
-                var foundContacts = findContacts(inputString);
+                input = readlineSync.question('please enter string for search');
+                var foundContacts = findContacts(input);
                 for (var i = 0; i < foundContacts.length; i++) {
                     console.log(contactToString(foundContacts[i]));
                 }
 
-                var foundGroups = findGroups(inputString);
+                var foundGroups = findGroups(input);
                 for (i = 0; i < foundGroups.length; i++) {
-                    console.log(foundGroups[i].name + "(" + foundGroups[i].id + ")");
+                    console.log(foundGroups[i].name + '(' + foundGroups[i].id + ')');
                 }
                 break;
 
             case MENU_DELETE_ITEM:
-                inputString = readlineSync.question('please enter item id for delete ');
-                if (deleteContact(input)) {
-                    console.log("contact deleted");
+                input = readlineSync.question('please enter item id for delete ');
+                if (input==0) {
+                    console.log('you cant delete root!!!');
                     break;
                 }
-                if (deleteAllChildrens(input)) {
-                    console.log("group deleted");
+                var deletedContact = getContactById(input);
+                var deletedGroup = getGroupById(input);
+
+                if (!(deletedContact||deletedGroup)){
+                    console.log('wrong ID');
                     break;
                 }
-                console.log("wrong ID");
+                if (deletedContact) {
+                    deleteContact(deletedContact);
+                    console.log('contact deleted');
+                    console.log('you in '+ currentGroup.name + ' now');
+                    break;
+                }
+                if (deletedGroup) {
+                    deleteAllChildrens(deletedGroup);
+                    console.log('group deleted');
+                    console.log('you in '+ currentGroup.name + ' now');
+                    break;
+                }
                 break;
+            case MENU_EXIT:
+                console.log('by.....');
+                return;
 
             default:
-                console.log("wrong!!!");
+                console.log('wrong input, try again!!!');
                 break;
         }
         readlineSync.question('press enter to continue');
-    } while (inputString != MENU_EXIT);
-
-    console.log("by.....");
+    } while (true);
 }
+
 function printMenu() {
-    console.log("1 -add new person");
-    console.log("2 -add new group");
-    console.log("3 -change current group");
-    console.log("4 -print current group");
-    console.log("5 -print all");
-    console.log("6 -find");
-    console.log("7 -delete item");
-    console.log("8 -exit");
+    console.log('1 -add new person');
+    console.log('2 -add new group');
+    console.log('3 -change current group');
+    console.log('4 -print current group');
+    console.log('5 -print all');
+    console.log('6 -find');
+    console.log('7 -delete item');
+    console.log('8 -exit');
 }
 
-function createContactFromString(inputString) {
-    var args = inputString.split(",");
-    var firstName = args[0];
-    var lastName = args[1];
-    var telephones = [];
-    for (var i = 2; i < args.length; i++) {
-        telephones.push(args[i]);
-    }
-    return createNewContact(firstName, lastName, telephones);
-}
-
-function addNewGroup(name) {
-    groups.push({id: getNextId(), name: name, parentId: currentGroupId});
+function inputNonEmptyField(message) {
+    do {
+        var invalidInput = true;
+        var inputString = readlineSync.question(message);
+        if (inputString) {
+            invalidInput = false;
+        }
+        else {
+            console.log('field cant be empty!');
+        }
+    } while (invalidInput);
+    return inputString.trim();
 }
 
 function changeCurrentGroup(name) {
-    if (name == "..") {
-        if (currentGroup.parentId) {
-
+    if (name == '..') {
+        if (currentGroup.parentId !== undefined) {
+            currentGroup = getGroupById(currentGroup.parentId);
+        }
+        else {
+            console.log('already top level!!!');
         }
         return;
     }
-    for (var i = 0; i < groups.length; i++) {
-        if (groups[i].parentId == currentGroupId && groups[i].name == name) {
-            console.log("found!");
-            currentGroupId = groups[i].id;
+    var subGroups = getSubGroups(currentGroup);
+    for (var i = 0; i < subGroups.length; i++) {
+        if (subGroups[i].name == name) {
+            currentGroup = subGroups[i];
             return;
         }
     }
-    console.log("sorry, no matches");
+    console.log('sorry, no matches');
 }
 
 function createNewContact(firstName, lastName, telephones) {
@@ -145,6 +164,7 @@ function createNewContact(firstName, lastName, telephones) {
     contact.firstName = firstName;
     contact.lastName = lastName;
     contact.telephones = telephones;
+    contact.groupId = currentGroup.id;
     return contact;
 }
 
@@ -157,14 +177,140 @@ function createNewGroup(name) {
 }
 
 function printGroup(group) {
-    console.log("****" + group.name + "******");
-    var contactsOfGroup = getContactsByGroupId(group.id);
-    var subGroups = getSubgroupsById(group.id);
+    console.log('your in  ' + group.name + '  group now');
+    var contactsOfGroup = getContactsOfGroup(group);
+    var subGroups = getSubGroups(group);
+    contactsOfGroup.sort(compareContacts);
+    subGroups.sort(compareGroups);
     for (var i = 0; i < contactsOfGroup.length; i++) {
         console.log(contactToString(contactsOfGroup[i]));
     }
     for (var i = 0; i < subGroups.length; i++) {
         console.log(groupToString(subGroups[i]));
+    }
+}
+
+function getNextId() {
+    return lastId++;
+}
+
+function printChildrens(group, tab) {
+    var contactsOfGroup = getContactsOfGroup(group);
+    var subGroups = getSubGroups(group);
+    contactsOfGroup.sort(compareContacts);
+    subGroups.sort(compareGroups);
+    for (var i = 0; i < contactsOfGroup.length; i++) {
+        console.log(tab + contactToString(contactsOfGroup[i]));
+    }
+    for (var i = 0; i < subGroups.length; i++) {
+        console.log(tab + groupToString(subGroups[i]));
+
+        printChildrens(subGroups[i], tab + '  ');
+    }
+}
+
+function compareContacts(a,b){
+    return a.firstName.localeCompare(b.firstName);
+}
+
+function compareGroups(a,b){
+    return a.name.localeCompare(b.name);
+}
+
+function findContacts(find) {
+    var result = [];
+    for (var i = 0; i < contacts.length; i++) {
+        if ((contacts[i].firstName.toLocaleLowerCase() == find.toLowerCase()) ||
+            (contacts[i].lastName.toLowerCase() == find.toLowerCase())) {
+            result.push(contacts[i]);
+        }
+    }
+    return result;
+}
+
+function findGroups(find) {
+    var result = [];
+    for (var i = 0; i < groups.length; i++) {
+        if (groups[i].name.toLowerCase() == find.toLowerCase()) {
+            result.push(groups[i]);
+        }
+    }
+    return result;
+}
+
+function deleteAllChildrens(group) {
+    deleteContactsOfGroup(group);
+    var subGroups = getSubGroups(group);
+    for (var i = 0; i < subGroups.length; i++) {
+            deleteAllChildrens(subGroups[i]);
+            deleteGroup(subGroups[i]);
+    }
+
+    deleteGroup(group);
+}
+
+function deleteContactsOfGroup(group){
+    getContactsOfGroup(group).forEach(deleteContact);
+}
+
+function deleteGroup(group){
+    if (group == currentGroup) {
+        currentGroup = getGroupById(group.parentId);
+    }
+    for (var i = 0; i < groups.length; i++) {
+        if (groups[i] == group) {
+            groups.splice(i, 1);
+            return;
+        }
+    }
+}
+
+function deleteContact(contact) {
+    for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i] == contact) {
+            contacts.splice(i, 1);
+            return;
+        }
+    }
+}
+
+function contactToString(contact) {
+    return '(' + contact.id +
+        ') first Name: ' + contact.firstName +
+        '; last name: ' + contact.lastName +
+        '; telephones: ' + contact.telephones + '.';
+}
+
+function groupToString(group) {
+    return '(' + group.id +
+        ') ' + group.name;
+}
+
+function getSubGroups(group) {
+    var result = [];
+    for (var i = 0; i < groups.length; i++) {
+        if (groups[i].parentId == group.id) {
+            result.push(groups[i]);
+        }
+    }
+    return result;
+}
+
+function getContactsOfGroup(group) {
+    var result = [];
+    for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i].groupId == group.id) {
+            result.push(contacts[i]);
+        }
+    }
+    return result;
+}
+
+function getContactById(id) {
+    for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i].id == id) {
+            return contacts[i];
+        }
     }
 }
 
@@ -175,106 +321,13 @@ function getGroupById(id) {
         }
     }
 }
-function getNextId() {
-    return lastId++;
-}
 
-function printChildrens(id, tab) {
-    var childrens = getChildrens(id);
-    printContacts(id, tab);
-    for (var i = 0; i < childrens.length; i++) {
-        console.log(tab + childrens[i].name + "(" + childrens[i].id + ")");
-        printChildrens(childrens[i].id, tab + "  ");
-    }
-
-}
-function findContacts(find) {
-    var result = [];
-    for (var i = 0; i < contacts.length; i++) {
-        if ((contacts[i].firstName == find) || (contacts[i].lastName == find)) {
-            result.push(contacts[i]);
-        }
-    }
-    return result;
-}
-function findGroups(find) {
-    var result = [];
-    for (var i = 0; i < groups.length; i++) {
-        if (groups[i].name == find) {
-            result.push(groups[i]);
-        }
-    }
-    return result;
-}
-function deleteContact(id) {
-    for (var i = 0; i < contacts.length; i++) {
-        if (contacts[i].id == id) {
-            contacts.splice(i, 1);
+function isDoubleName(name) {
+    var subGroups = getSubGroups(currentGroup);
+    for (var i = 0; i < subGroups.length; i++) {
+        if (subGroups[i].name == name) {
             return true;
         }
     }
     return false;
-}
-
-function deleteAllChildrens(id) {
-    for (var j = 0; j < contacts.length; j++) {
-        if (contacts[j].groupId == id) {
-            contacts.splice(j, 1);
-            j--;
-        }
-    }
-    for (var i = 0; i < groups.length; i++) {
-        if (groups[i].parentId == id) {
-            deleteAllChildrens(groups[i].id);
-            groups.splice(i, 1);
-            i--;
-        }
-    }
-    for (i = 0; i < groups.length; i++) {
-        if (groups[i].id == id) {
-            groups.splice(i, 1);
-            return true;
-        }
-    }
-    return false;
-}
-function printContacts(id, tab) {
-    for (var i = 0; i < contacts.length; i++) {
-        if (contacts[i].groupId == id) {
-            console.log(tab + "-" + contacts[i].firstName + " " +
-                contacts[i].lastName + " " +
-                contacts[i].phone + "(" + contacts[i].id + ")");
-        }
-    }
-}
-
-function contactToString(contact) {
-    return contact.firstName + " " +
-        contact.lastName + " " +
-        contact.phone + "(" +
-        contact.id + ")";
-}
-
-function groupToString(group) {
-    return group.name + "(" + group.id + ")";
-}
-
-function getSubgroupsById(id) {
-    var result = [];
-    for (var i = 0; i < groups.length; i++) {
-        if (groups[i].parentId == id) {
-            result.push(groups[i]);
-        }
-    }
-    return result;
-}
-
-function getContactsByGroupId(id) {
-    var result = [];
-    for (var i = 0; i < contacts.length; i++) {
-        if (contacts[i].groupId == id) {
-            result.push(contacts[i]);
-        }
-    }
-    return result;
 }
